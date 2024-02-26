fd184a37cec540b0f53e7b725f518fc60fcf0cbb
Axel Jensen <axel.jensen@intevity.com>
2024-02-26 16:11:32 -0600

    adding to readme

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-# Mantine Vite template
+# Demo Frontend
+
+@see below to run the project locally
+
+This project is deployed using AWS Amplify to my personal
+console. [AWS Amplify Deployed](https://main.d1zu60gaf1jr0y.amplifyapp.com/)
+
+@see *src/components/registration*
+
+### Things to look at:
+
+- *src/ui* custom ui components that wraps mantine's text input, and offers restrictions, and customization.
+- PhoneNumberInput reuses TextInput
+- *src/utils* has some formatters and helpers I've built in them.
+
+No State management libraries or Api's are used or wired up
+
+Coding exercise from a frontend perspective: Healthcare providers request to be part of the Availity system. Using
+React framework, create a registration user interface so healthcare providers can electronically join Availity. The
+following data points should be collected:
+
+- First and Last Name
+- NPI number
+- Business Address
+- Telephone Number
+- Email address
+
+---
+
+# Built with Mantine Vite template
 
 ## Features
 
@@ -7,7 +36,8 @@ This template comes with the following features:
 - [PostCSS](https://postcss.org/) with [mantine-postcss-preset](https://mantine.dev/styles/postcss-preset)
 - [TypeScript](https://www.typescriptlang.org/)
 - [Storybook](https://storybook.js.org/)
-- [Vitest](https://vitest.dev/) setup with [React Testing Library](https://testing-library.com/docs/react-testing-library/intro)
+- [Vitest](https://vitest.dev/) setup
+  with [React Testing Library](https://testing-library.com/docs/react-testing-library/intro)
 - ESLint setup with [eslint-config-mantine](https://github.com/mantinedev/eslint-config-mantine)
 
 ## npm scripts
--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useState } from 'react';
-import { Button, Container, Flex, Textarea } from '@mantine/core';
+import { Button, Container, Flex, Textarea, Title } from '@mantine/core';
 import { PhoneNumberInput, TextInput } from '@/ui';
 import { RESTRICT_REGEX } from '@/utils';
 
@@ -32,64 +32,70 @@ function RegistrationForm() {
     };
 
     return (
-        <Container size="sm">
-            <form onSubmit={handleSubmit}>
-                <TextInput
-                  label="First Name"
-                  name="firstName"
-                  value={formData.firstName}
-                  restrict={RESTRICT_REGEX.NAME_CASE}
-                  onChange={handleChange}
-                  required
-                />
-                <TextInput
-                  label="Last Name"
-                  name="lastName"
-                  value={formData.lastName}
-                  restrict={RESTRICT_REGEX.NAME_CASE}
-                  onChange={handleChange}
-                  required
-                />
-                <TextInput
-                  label="NPI Number"
-                  name="npiNumber"
-                  value={formData.npiNumber}
-                  onChange={handleChange}
-                  required
-                />
-                <Textarea
-                  label="Business Address (typically this would be multiple fields, with zipcode and state validation, etc...)"
-                  name="businessAddress"
-                  value={formData.businessAddress}
-                  onChange={handleChange}
-                  required
-                />
-                <PhoneNumberInput
-                  label="Telephone Number"
-                  name="telephoneNumber"
-                  value={formData.telephoneNumber}
-                  onChange={handleChange}
-                  required
-                />
-                <TextInput
-                  label="Email Address"
-                  name="emailAddress"
-                  type="email"
-                  value={formData.emailAddress}
-                  onChange={handleChange}
-                  required
-                />
-                <Flex mt={20}>
-                    <Button type="submit" variant="filled" color="blue">
-                        Submit
-                    </Button>
-                </Flex>
+        <>
+            <Container size="sm">
+                <Title>Register</Title>
+                <Container pt={25} px={0} mx={0}>
 
-                <pre>
-                    {JSON.stringify(formData, null, 4)}
-                </pre>
-            </form>
-        </Container>
+                    <form onSubmit={handleSubmit}>
+                        <TextInput
+                          label="First Name"
+                          name="firstName"
+                          value={formData.firstName}
+                          restrict={RESTRICT_REGEX.NAME_CASE}
+                          onChange={handleChange}
+                          required
+                        />
+                        <TextInput
+                          label="Last Name"
+                          name="lastName"
+                          value={formData.lastName}
+                          restrict={RESTRICT_REGEX.NAME_CASE}
+                          onChange={handleChange}
+                          required
+                        />
+                        <TextInput
+                          label="NPI Number"
+                          name="npiNumber"
+                          value={formData.npiNumber}
+                          onChange={handleChange}
+                          required
+                        />
+                        <Textarea
+                          label="Business Address (typically this would be multiple fields, with zipcode and state validation, etc...)"
+                          name="businessAddress"
+                          value={formData.businessAddress}
+                          onChange={handleChange}
+                          required
+                        />
+                        <PhoneNumberInput
+                          label="Telephone Number"
+                          name="telephoneNumber"
+                          value={formData.telephoneNumber}
+                          onChange={handleChange}
+                          required
+                        />
+                        <TextInput
+                          label="Email Address"
+                          name="emailAddress"
+                          type="email"
+                          value={formData.emailAddress}
+                          onChange={handleChange}
+                          required
+                        />
+                        <Flex mt={20}>
+                            <Button type="submit" variant="filled" color="blue">
+                                Submit
+                            </Button>
+                        </Flex>
+
+                        <pre>
+                            {JSON.stringify(formData, null, 4)}
+                        </pre>
+                    </form>
+                </Container>
+            </Container>
+        </>
     );
 }
 
